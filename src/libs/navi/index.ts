8a6ee57f147d10e445529d09919b7fce88dca5d2
Zcy <zcy_chao1024@163.com>
2025-02-09 14:12:05 +0800

    use Dex.BLUEFIN to swap

--- src/libs/navi/index.ts
+++ src/libs/navi/index.ts
@@ -1,10 +1,10 @@
 'use server'
 
-import {NAVX, getQuote as get} from "navi-sdk";
+import {getQuote as get} from "navi-sdk";
 import {Dex} from "navi-sdk/dist/types";
 
 export async function getQuote(from: string, to: string, amount: number) {
-    return await get(NAVX.address, "0x2::sui::SUI", amount, "", {
+    return await get(from, to, amount, "", {
         dexList: [Dex.BLUEFIN]
     });
 }
